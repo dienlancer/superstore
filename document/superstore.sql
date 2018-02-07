0450491f77717e6183a048eb9d116ba742b64435
trietnk01 <trietnk01@gmail.com>
2018-02-07 17:24:49 +0700

    cập nhật

--- document/superstore.sql
+++ document/superstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th2 07, 2018 lúc 08:28 AM
+-- Thời gian đã tạo: Th2 07, 2018 lúc 11:23 AM
 -- Phiên bản máy phục vụ: 10.1.29-MariaDB
 -- Phiên bản PHP: 7.0.26
 
